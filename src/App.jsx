7fcaa5f23dfbea202d3c6a74f9e16f639fb95203
hayitmurod707 <hayitmurod707@gmail.com>
2022-07-30 16:32:33 +0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import Source from './lamborghini.mp4';
+import Video from './Video';
+const StyledElement = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	& .video-container {
+		width: 700px;
+	}
+`;
+const App = () => (
+	<StyledElement>
+		<div className="video-container">
+			<Video source={Source} isEncryption={false} />
+		</div>
+	</StyledElement>
+);
+export default App;
---
+++ src/Video.jsx
@@ -0,0 +1,138 @@
+import Hls from 'hls.js';
+// import Poster from 'Images/poster.jpg';
+import Plyr from 'plyr';
+import { bool, string } from 'prop-types';
+import React, { useEffect, useRef } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+const Styles = createGlobalStyle`
+   :root {
+      --plyr-color-main: #466fff !important;
+      --plyr-control-radius: 8px !important;
+   }
+`;
+const StyledElement = styled.div`
+	& .plyr {
+		-moz-border-radius: 20px !important;
+		-ms-border-radius: 20px !important;
+		-o-border-radius: 20px !important;
+		-webkit-border-radius: 20px !important;
+		border-radius: 20px !important;
+		font-family: 'Gilroy', sans-serif;
+		overflow: hidden !important;
+		& .plyr__controls {
+			justify-content: flex-start;
+			padding: 20px 10px 10px 10px;
+			& .plyr__controls__item:first-child {
+				margin: 0;
+			}
+			& .plyr__controls__item.plyr__progress__container {
+				left: 0;
+				margin: 0;
+				padding: 0 10px;
+				position: absolute;
+				top: 0;
+				width: 100%;
+			}
+			& .plyr__progress input[type='range'],
+			& .plyr__volume input[type='range'] {
+				cursor: pointer;
+				&::-webkit-slider-runnable-track {
+					border-radius: 2px;
+					height: 3px;
+				}
+				&::-webkit-slider-thumb {
+					border-radius: 50%;
+					height: 12px;
+					width: 12px;
+				}
+			}
+			& .plyr__time {
+				font-family: 'Gilroy', sans-serif !important;
+				font-size: 15px !important;
+				font-weight: 600;
+			}
+			& .plyr__time--current {
+				margin: 0 auto 0 0;
+			}
+		}
+		& .plyr__poster {
+			background-size: cover;
+		}
+	}
+`;
+const Video = ({ source, isEncryption }) => {
+	const ref = useRef(null);
+	const player = useRef(null);
+	useEffect(() => {
+		const lang = localStorage.getItem('i18nextLng') || 'uz';
+		const defaultOptions = {
+			controls: [
+				'play-large',
+				'play',
+				'mute',
+				'current-time',
+				'progress',
+				'duration',
+				'airplay',
+				'capture',
+				'volume',
+				'settings',
+				'fullscreen',
+			],
+			settings: ['quality', 'speed'],
+			speed: {
+				selected: 1,
+				options: [0.5, 1, 1.5, 2],
+			},
+			i18n: {
+				normal: lang === 'uz' ? 'Normal' : 'Нормальный',
+				quality: lang === 'uz' ? 'Tasvir sifati' : 'Качество',
+				speed: lang === 'uz' ? 'Tasvir tezligi' : 'Скорость',
+			},
+		};
+		if (Hls.isSupported() && ref.current && isEncryption) {
+			const updateQuality = newQuality => {
+				window.hls.levels.forEach((level, levelIndex) => {
+					if (level.height === newQuality) {
+						window.hls.currentLevel = levelIndex;
+					}
+				});
+			};
+			const hls = new Hls();
+			hls.loadSource(source);
+			hls.on(Hls.Events.MANIFEST_PARSED, () => {
+				const availableQualities = hls.levels.map(({ height }) => height);
+				defaultOptions.quality = {
+					default: availableQualities[0],
+					forced: true,
+					onChange: e => updateQuality(e),
+					options: availableQualities,
+				};
+				player.current = new Plyr(ref.current, defaultOptions);
+			});
+			hls.attachMedia(ref.current);
+			window.hls = hls;
+		} else {
+			player.current = new Plyr(ref.current, defaultOptions);
+		}
+	}, [source]);
+	return (
+		<StyledElement>
+			<video
+				controls
+				crossOrigin="true"
+				height="auto"
+				playsInline
+				// poster={Poster}
+				preload="none"
+				ref={ref}
+				src={source}
+				width="100%"
+			/>
+			<Styles />
+		</StyledElement>
+	);
+};
+Video.defaultProps = { source: '', isEncryption: false };
+Video.propTypes = { source: string, isEncryption: bool };
+export default Video;
